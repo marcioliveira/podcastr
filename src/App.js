1e4de365f61793e9a991361885941a3e74cf3fcd
marcioliveira <marcioruelaoliveira@gmail.com>
2021-04-19 19:31:05 -0300

    segundo commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import Button from "./Button";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Button title="Button 1"/>
+      <Button title="Button 2"/>
+      <Button title="Button 3"/>
+      <Button title="Button 4"/>
+      <Button title="Button 5"/> 
+    </>    
   );
 }
 
